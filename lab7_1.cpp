bb89f320661cb557c7ba5c7d3133c5dfa6383a54
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-21 16:02:16 +0700

    golden age

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
+/*int main()
 {
   char rank;
   cout << "Input your rank:";
@@ -13,5 +13,35 @@ int main()
     case 'C': cout << "You have received 2000 coins.\n";
     case 'D': cout << "You have received very KAK items.\n";
   }
+  return 0;
+}*/
+
+int main(){
+  char rank;
+  cout << "Input your rank:";
+  cin >> rank;
+
+  if (rank == 'S') {
+    cout << "You have received Super Ultra Rare Unit!!!\n";
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+  } else if (rank == 'A'){
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+  } else if (rank == 'B'){
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+  } else if (rank == 'C'){
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
+  } else if (rank == 'D'){
+    cout << "You have received very KAK items.\n";
+  }
+
   return 0;
 }
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,49 @@
 //status = "ONE-NIGHT-STAND";
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int age;
+    string status;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 20){
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 160){
+            status = "UNFRIEND";
+        } else if (height < 175){
+            status = "FRIEND";
+
+        } else {
+            int wealth;
+            cout << "Enter your wealth: ";
+            cin >> wealth;
+            if (wealth > 69000000){
+                status = "MARRIED";
+            } else {
+                status = "ONE-NIGHT-STAND";
+            }
+        }
+
+    } else if (age < 30){
+        int wealth;
+        cout << "Enter you wealth: ";
+        cin >> wealth;
+        if (wealth > 10000000){
+            status = "BEST FRIEND";
+        } else {
+            status = "UNFRIEND";
+        }
+
+    } else {
+        status = "UNFRIEND";
+    }
+
+    cout << "status = " << status << endl;
+}
